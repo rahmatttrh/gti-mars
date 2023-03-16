7f32369bebf29098ef6fc3344c339b1267fe8cc7
nurdiansah <nurdiansah14045@gmail.com>
2023-03-16 11:03:17 +0700

    CMS Route Type

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
          RoleSeeder::class,
          UserSeeder::class,
          VesselSeeder::class,
+         RouteTypeSeeder::class,
          PortSeeder::class,
          ScheduleSeeder::class,
          LogisticSeeder::class,
