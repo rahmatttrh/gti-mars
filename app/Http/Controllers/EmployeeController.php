9f595179f6432628087b10d2b5cb5c5feea0546d
“rahmatttrh” <rahmattrust@gmail.com>
2023-05-16 10:24:37 +0700

    fixing email notif

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -61,7 +61,7 @@ class EmployeeController extends Controller
          'link' => 'http://103.21.206.66:8005/'
       ];
 
-      Mail::to("rahmattrust@gmail.com")->send(new WelcomeEmail($data));
+      Mail::to($employee->email)->send(new WelcomeEmail($data));
       // Mail::to($employee->email)->send(new WelcomeEmail($data));
 
 
