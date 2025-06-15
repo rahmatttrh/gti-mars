e11710b9fc0fcd2158c97907905f0321702ab159
Rahmat Hidayat <rahmattrust@gmail.com>
2025-06-15 21:12:46 +0700

    update sidebar

--- resources/views/layouts/urbix/app.blade.php
+++ resources/views/layouts/urbix/app.blade.php
@@ -48,7 +48,7 @@
         <div class="container-fluid w-100">
             <div class="d-flex justify-content-between align-items-center">
                 <div class="d-inline-flex align-items-center gap-2">
-                    <a href="index.html" class="align-items-end logo-main d-none me-5">
+                    <a href="/" class="align-items-end logo-main d-none me-5">
                         <img height="35" width="34" class="logo-dark" alt="Dark Logo" src="{{asset('urbix/images/logo-md.png')}}">
                         <h3 class="text-body-emphasis fw-bolder mb-0 ms-1">MARS</h3>
                     </a>
@@ -172,7 +172,7 @@
       </div>    <aside class="pe-app-sidebar" id="sidebar">
         <div class="pe-app-sidebar-logo px-6 d-flex align-items-center position-relative">
             <!--begin::Brand Image-->
-            <a href="index.html" class="d-flex align-items-end logo-main">
+            <a href="/" class="d-flex align-items-end logo-main">
                 <img height="35" width="34" class="logo-dark" alt="Dark Logo" src="{{asset('urbix/images/logo-md.png')}}">
                 <img height="35" width="34" class="logo-light" alt="Light Logo" src="{{asset('urbix/images/logo-md-light.png')}}">
                 <h3 class="text-body-emphasis fw-bolder mb-0 ms-1">MARS</h3>
@@ -193,7 +193,7 @@
                         </a>
                         <ul class="pe-slide-menu collapse" id="collapseDashboards">
                             <li class="pe-slide-item">
-                                <a href="index.html" class="pe-nav-link">
+                                <a href="/" class="pe-nav-link">
                                     Monthly
                                 </a>
                             </li>
