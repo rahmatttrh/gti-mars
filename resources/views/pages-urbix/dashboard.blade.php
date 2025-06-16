4de7a499d0c2687ea92c526fc0d30d1a558644bc
Rahmat Hidayat <rahmattrust@gmail.com>
2025-06-16 17:58:25 +0700

    add intermilan

--- resources/views/pages-urbix/dashboard.blade.php
+++ resources/views/pages-urbix/dashboard.blade.php
@@ -57,7 +57,7 @@
                            <!-- end card-body -->
                          </div> --}}
                      </div>
-                     <div class="col-xxl-3">
+                     {{-- <div class="col-xxl-3">
                         <div class="row">
                             <div class="col-xxl-12 col-md-6">
                               <div class="card card-h-100">
@@ -83,7 +83,7 @@
                               </div>
                             </div>
                         </div>
-                    </div>
+                    </div> --}}
 
                      
                      <div class="col-xxl-3">
@@ -109,6 +109,138 @@
                                  </div>
                              </div>
                          </div>
+                     </div>
+                  </div>
+                  <div class="card ">
+                     <!--start::card-->
+                     <div class="card-header">
+                         <h5 class="card-title mb-0"> Intermilan </h5>
+                     </div>
+                     <div class="card-body">
+                        <div class="table-responsive" >
+                              <table class="table  table-sm  mb-0">
+                                 <thead>
+                                    <tr>
+                                       <th>Station</th>
+                                       <th>Activity</th>
+                                       <th>Location</th>
+                                       <th>Date</th>
+                                       <th>Boat</th>
+                                       @foreach ($dates as $date)
+                                          <th class="text-center">{{formatDateOnly($date)}}</th>
+                                       @endforeach
+                                    </tr>
+                                 </thead>
+                                 <tbody>
+               
+                                    @if ($cargoItems->count() > 0)
+                                       @foreach ($cargoItems as $item)
+                                       @if ($item->request_id != null)
+               
+                                       @if ($item->request->user->getPort()->func == 'DWI')
+                                          <tr class="border" style="background-color: rgb(242, 248, 221)">
+                                             @else
+                                             <tr style="background-color: rgb(230, 221, 252)">
+                                          @endif
+                                          <td  class="text-uppercase bg-light">
+                                             {{$item->request->user->username}}
+                                          </td>
+                                       
+                                             <td >
+                                                {{$item->description}}
+                                             </td>
+                                             <td >
+                                                {{-- @if ($request->activity_id < 5)
+                                                {{$request->origin->code}} to {{$request->destination->code}}
+                                                @else
+                                                
+                                                @endif --}}
+                                                {{$item->request->origin->code}}
+                                                {{-- @if ($request->origin->port_id != null)
+                                                   ({{$request->origin->port->code}})
+                                                   
+                                                @endif --}}
+                                                to {{$item->request->destination->code}}
+                                             </td>
+                                             <td>{{formatDate($item->request->date)}}</td>
+                                             <td class="d-flex align-items-center">
+                                                @if ($item->request->activity_id == 1 || $item->request->activity_id == 2)
+                                                   <form action="{{route('intermilan.marine.select.schedule.list')}}" method="POST" class="d-flex">
+                                                      @csrf
+                                                      @method('PUT')
+                                                      <input type="text" name="cargoItemId" id="cargoItemId" value="{{$item->id}}" hidden>
+                                                      <select style="width: 150px" name="schedule" id="schedule" required>
+                                                         <option value="" selected disabled>Select Schedule {{$item->schedule_id}}</option>
+                                                         {{-- <option value=""><a href="/">OK</a></option> --}}
+                                                         @foreach ($schedules as $sche)
+                                                            @if ($sche->class == 'Cargo' || $sche->class == 'Crew')
+                                                            <option {{$item->schedule_id == $sche->id ? 'selected' : ''}} value="{{$sche->id}}">{{$sche->vessel->name}} - {{formatDate($sche->date)}}</option>
+                                                            @endif
+                                                            
+                                                         @endforeach
+                                                         
+                                                      </select>
+                                                      @if ($item->request->status == 1)
+                                                         <button class="btn btn-sm border btn-info">
+                                                         @else
+                                                         <button class="btn btn-sm border btn-light">
+                                                      @endif
+                                                      {{-- <button class="btn btn-sm border btn-info"> --}}
+                                                         {{-- <i class="fa fa-save"></i> --}}
+                                                         Assign
+                                                      </button>
+                                                   </form>
+                                                   @else
+                                                   {{-- <a href="{{route('schedule.detail', enkripRambo($request->schedule_id))}}"> {{$request->schedule->vessel->name ?? 'Not Available'}}</a> --}}
+                                                   <form action="{{route('intermilan.marine.select.vessel')}}" method="POST" class="d-flex">
+                                                      @csrf
+                                                      @method('PUT')
+                                                      <input type="text" name="requestId" id="requestId" value="{{$item->request->id}}" hidden>
+                                                      <input type="text" name="scheduleId" id="scheduleId" value="{{$item->request->schedule->id}}" hidden>
+                                                      <select style="width: 150px" name="vessel" id="vessel">
+                                                         <option value="" selected disabled>Select Vessel</option>
+                                                         @foreach ($vessels as $vessel)
+               
+                                                            <option {{$item->request->schedule->vessel_id == $vessel->id ? 'selected' : ''}} value="{{$vessel->id}}">{{$vessel->name ?? '-'}}</option>
+                                                         @endforeach
+                                                         
+                                                      </select>
+                                                      @if ($item->request->schedule->vessel_id != null)
+                                                         <button type="submit" class="btn btn-sm border">Assign</button>
+                                                         @else
+                                                         <button type="submit" class="btn btn-sm border btn-info">Assign</button>
+                                                      @endif
+                                                      
+                                                   </form>
+                                                @endif
+                                                
+                                                
+                                                
+                                                
+                                             </td>
+                                             @foreach ($dates as $date)
+                                                @if ($date == $item->date)
+                                                   <x-status-stisla.item-vessel :item="$item" />
+                                                   
+                                                   
+                                                @else
+                                                <td class="text-center">- </td>
+                                                @endif
+                                                
+                                             @endforeach
+                                             
+                                          </tr>
+                                          @endif
+                                       @endforeach
+                                       @else
+                                       <tr class="text-center">
+                                          <td colspan="4" class="text-center" style="height: 35px">Tidak ada data Intermilan di rentang waktu yang dipilih</td>
+                                       </tr>
+                                    @endif
+                                    
+                                 </tbody>
+                              </table>
+                        </div>
                      </div>
                   </div>
                </div>
