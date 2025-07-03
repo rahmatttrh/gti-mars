08c30ae1e60232bd0e62976f284494f69554c71e
Rahmat H <rahmattrust@gmail.com>
2025-07-03 10:07:46 +0700

    from server

--- resources/views/pages-stisla/vdr/detail-new.blade.php
+++ resources/views/pages-stisla/vdr/detail-new.blade.php
@@ -160,11 +160,12 @@
                            {{-- <div class="card bg-warning">
                               <div class="card-boy"></div>
                            </div> --}}
-                           <div class="btn btn-warning  ml-2 text-dark" style="background-color: rgb(226, 236, 151);" >
-                               <span class="badge badge-dark border">!</span> Harap isi kolom berwarna kuning
-                           </div>
+                           
                         </div>
-                        <hr>
+                        <div class="btn btn-warning  ml-2 mt-2 text-dark" style="background-color: rgb(226, 236, 151);" >
+                           <span class="badge badge-dark border">!</span> Harap isi kolom berwarna kuning
+                       </div>
+                        
                      
 
                      
