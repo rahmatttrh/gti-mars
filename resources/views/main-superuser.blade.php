92cc28bd560337087d4825465cc86d2369181dcc
Rahmat Hidayat <rahmattrust@gmail.com>
2025-09-08 09:18:29 +0700

    update dashboard admin

--- resources/views/main-superuser.blade.php
+++ resources/views/main-superuser.blade.php
@@ -7,7 +7,7 @@
    <section class="section">
       <div class="section-body">
          <div class="row"> 
-            <div class="col-md-7">
+            <div class="col-md-12">
                {{-- <div class="row">
                   <div class="col-md-6">
                      <div class="card card-statistic-1 border">
@@ -51,43 +51,89 @@
                      
                   </div> --}}
                   <div class="card-body">
-                     <div class="badge badge-info mb-2">
-                        ALL VDR
-                     </div>
-                     {{-- <table class="display  border">
-                        <tbody>
-                           <tr>
-                              <th>All Vessel Daily Report</th>
-                           </tr>
-                        </tbody>
-                     </table> --}}
-                     
-                        <table class="datatables-vdr">
-                           
-                           <thead>
-                              
-                              <tr>
-                                 <th>ID</th>
-                                 <th>Vessel</th>
-                                 <th>Date</th>
-                                 {{-- <th>Date</th> --}}
-                                 <th>Status</th>
-                              </tr>
-                           </thead>
-                           <tbody>
-                              @foreach ($allVdrs as $vdr)
-                                 <tr >
-                                    <td>{{$vdr->id}}</td>
-                                    <td class="text-truncate" ><a href="{{route('vdr.show.spa', [enkripRambo($vdr->id), enkripRambo('index')])}}">{{$vdr->code}}</a></td>
-                                    <td>{{$vdr->date}}</td>
-                                    {{-- <td>{{formatDate($sche->date)}}</td> --}}
-                                    <td class="text-truncate">
-                                       <x-status-stisla.vdr :vdr="$vdr" />
-                                    </td>
+                     <div class="row">
+                        <div class="col-md-9">
+                           <div class="badge badge-info mb-2">
+                              ALL VDR
+                           </div>
+                           {{-- <table class="display  border">
+                              <tbody>
+                                 <tr>
+                                    <th>All Vessel Daily Report</th>
                                  </tr>
-                              @endforeach
-                           </tbody>
-                        </table>
+                              </tbody>
+                           </table> --}}
+                           
+                              <table class="datatables-vdr">
+                                 
+                                 <thead>
+                                    
+                                    <tr>
+                                       <th>ID</th>
+                                       <th>Vessel</th>
+                                       <th>Date</th>
+                                       {{-- <th>Date</th> --}}
+                                       <th>Status</th>
+                                    </tr>
+                                 </thead>
+                                 <tbody>
+                                    @foreach ($allVdrs as $vdr)
+                                       <tr >
+                                          <td>{{$vdr->id}}</td>
+                                          <td class="text-truncate" ><a href="{{route('vdr.show.spa', [enkripRambo($vdr->id), enkripRambo('index')])}}">{{$vdr->code}}</a></td>
+                                          <td>{{$vdr->release_date}}</td>
+                                          {{-- <td>{{formatDate($sche->date)}}</td> --}}
+                                          <td class="text-truncate">
+                                             <x-status-stisla.vdr :vdr="$vdr" />
+                                          </td>
+                                       </tr>
+                                    @endforeach
+                                 </tbody>
+                              </table>
+                        </div>
+                        <div class="col-md-3">
+                           <div class="card shadow-lg">
+                              <div class="card-body p-0">
+                                 {{-- <div class="badge badge-info mb-2">Log Activity</div> --}}
+                                 <div  class="table-responsive overflow-auto " style="height: 350px" >
+                                 <table class=""   >
+                                    <thead>
+                                       <tr class="border">
+                                          <th>Log Activity</th>
+                                          {{-- <th>User</th> --}}
+                                          {{-- <th>Action</th> --}}
+                                       </tr>
+                                    </thead>
+                                    <tbody>
+                                       @foreach ($logs as $log)
+                                          <tr class="border">
+                                             <td class="">
+                                                {{-- <div class="badge badge-light"> --}}
+                                                   {{-- {{$log->created_at}}  --}}
+                                                   
+                                                   {{$log->user->name ?? ''}} : {{$log->action}}<br>
+                                                   <small>{{$log->created_at}} </small>
+                                                {{-- </div> --}}
+                                               
+                                                
+                                                 
+                                             </td>
+                                             {{-- <td class="text-truncate" style="width: 90px"></td> --}}
+                                             {{-- <td></td> --}}
+                                             {{-- <td class="text-truncate" style="max-width: 100px"></td> --}}
+                                             {{-- <td></td> --}}
+                                             
+                                          </tr>
+                                       @endforeach
+                                    </tbody>
+                                 </table>
+                                 </div>
+                                 {{-- {{ $logs->links() }} --}}
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                     
                      
                   </div>
                </div>
@@ -119,46 +165,9 @@
                @endif --}}
       
             </div>
-            <div class="col-md-5">
+            <div class="col-md-4">
                
-               <div class="card shadow-lg">
-                  <div class="card-body">
-                     <div class="badge badge-info mb-2">Log Activity</div>
-                     <div  class="table-responsive overflow-auto " style="height: 450px" >
-                     <table class=""   >
-                        <thead>
-                           <tr class="border">
-                              <th>Time</th>
-                              {{-- <th>User</th> --}}
-                              <th>Action</th>
-                           </tr>
-                        </thead>
-                        <tbody>
-                           @foreach ($logs as $log)
-                              <tr class="border">
-                                 <td class="text-truncate">
-                                    {{-- <div class="badge badge-light"> --}}
-                                       {{-- {{$log->created_at}}  --}}
-                                       {{$log->created_at}} <br>
-                                       {{$log->user->name ?? ''}}
-                                    {{-- </div> --}}
-                                   
-                                    
-                                     
-                                 </td>
-                                 {{-- <td class="text-truncate" style="width: 90px"></td> --}}
-                                 <td>{{$log->action}}</td>
-                                 {{-- <td class="text-truncate" style="max-width: 100px"></td> --}}
-                                 {{-- <td></td> --}}
-                                 
-                              </tr>
-                           @endforeach
-                        </tbody>
-                     </table>
-                     </div>
-                     {{-- {{ $logs->links() }} --}}
-                  </div>
-               </div>
+              
                
               
             </div>
