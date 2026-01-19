6d9a3386df578a573df8ccd7b1c92da792d83a36
Rahmat H <rahmattrust@gmail.com>
2026-01-19 08:52:32 +0700

    win

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-  <title>DSP - 500</title>
+  <title>MARS - 500</title>
 
   <!-- General CSS Files -->
   <link rel="stylesheet" href="{{asset('stisla/modules/bootstrap/css/bootstrap.min.css')}}">
